6e4c11c5b31f6b3823f4ef451aab3bf6c4fa8319
Peter Kay <contact@kaypeter.com>
2021-01-18 23:42:43 -0500

    add editline library and implement history

--- intprompt.c
+++ intprompt.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <editline/readline.h>
+#include <editline/history.h>
 
 /* Declare a buffer for user input of size 2048 */
 static char input[2048];
@@ -11,10 +15,13 @@ int main(int argc, char** argv) {
   /* Never ending loop */
   while (1) {
 
-    fputs("lispy> ", stdout);
-    fgets(input, 2048, stdin);
+    char* input = readline("lispy> ");
+
+    add_history(input);
 
     printf("No you're a %s", input);
+
+    free(input)
   }
   return 0;
 }
